8b89bd63a7dbc9627e76e99d8bb22e601491b466
Ankit <ankitumredkar77@gmail.com>
2024-06-08 22:34:27 +0530

    Initial commit

--- lib/Screens/HomPage/homepage.dart
+++ lib/Screens/HomPage/homepage.dart
@@ -18,64 +18,303 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: bgColor,
         body: Padding(
           padding: const EdgeInsets.fromLTRB(20, 18, 20, 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: height * 0.07,
-                margin: const EdgeInsets.only(bottom: 14),
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: const Color(0xff262B31)),
-                child: TextField(
-                  textAlignVertical: TextAlignVertical.center,
-                  style: const TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    prefixIcon: const Icon(
-                      Icons.search,
-                      color: Color(0xff8A98B7),
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // todo -------------------------> TextField
+                Container(
+                  height: height * 0.07,
+                  margin: const EdgeInsets.only(bottom: 14),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: const Color(0xff262B31)),
+                  child: TextField(
+                    textAlignVertical: TextAlignVertical.center,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      prefixIcon: const Icon(
+                        Icons.search,
+                        color: Color(0xff8A98B7),
+                      ),
+                      hintText: 'Type Something Here',
+                      hintStyle: TextStyle(
+                          color: const Color(0xff94959B),
+                          fontSize: width * 0.046,
+                          fontFamily: 'gc_m'),
                     ),
-                    hintText: 'Type Something Here',
-                    hintStyle: TextStyle(
-                        color: const Color(0xff94959B),
-                        fontSize: width * 0.046,
-                        fontFamily: 'gc_m'),
                   ),
                 ),
-              ),
-              Text(
-                'Popular',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'gc_m',
-                    fontSize: width * 0.062),
-              ),
-              SingleChildScrollView(
-                physics: const BouncingScrollPhysics(),
-                scrollDirection: Axis.horizontal,
-                child: Row(
+                // todo -------------------------> Popular
+                headingOfHomepage(width, 'Popular'),
+                SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
                     children: List.generate(
-                  popularImage.length,
-                  (index) => Column(
+                      popularImage.length,
+                      (index) => Column(
+                        children: [
+                          Container(
+                            height: height * 0.13,
+                            width: width * 0.3,
+                            margin: const EdgeInsets.only(
+                                top: 10, right: 12, bottom: 4),
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage(popularImage[index]),
+                                    fit: BoxFit.cover),
+                                borderRadius: BorderRadius.circular(12),),
+                          ),
+                          Text(
+                            popularText[index],
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'gc_m',
+                                fontSize: width * 0.043),
+                          ),
+                          const SizedBox(height: 25),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                // todo -------------------------> Motivational
+                headingOfHomepage(width, 'Motivational'),
+                SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: List.generate(
+                      motivationImage.length,
+                      (index) => Column(
+                        children: [
+                          Container(
+                            height: height * 0.13,
+                            width: width * 0.3,
+                            margin: const EdgeInsets.only(
+                                top: 10, right: 12, bottom: 4),
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage(motivationImage[index]),
+                                    fit: BoxFit.cover),
+                                borderRadius: BorderRadius.circular(12)),
+                          ),
+                          Text(
+                            motivationText[index],
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'gc_m',
+                                fontSize: width * 0.043),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 25),
+                // todo -------------------------> Random Quotes
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    headingOfHomepage(width, 'Random Quotes'),
+                    Text(
+                      'NEXT',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontFamily: 'gc_m',
+                          fontSize: width * 0.038),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 15),
+                AnimatedContainer(
+                  duration: const Duration(milliseconds: 30),
+                  height: height * 0.43,
+                  width: width,
+                  padding: EdgeInsets.only(bottom: height * 0.014),
+                  decoration: BoxDecoration(
+                    color: Colors.black54,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Container(
-                        height: height * 0.13,
-                        width: width * 0.3,
-                        margin: const EdgeInsets.only(top: 10, right: 12,bottom: 4),
+                      AnimatedContainer(
+                        height: height * 0.34,
+                        duration: const Duration(milliseconds: 30),
                         decoration: BoxDecoration(
-                            image: DecorationImage(
-                                image: AssetImage(popularImage[index]), fit: BoxFit.cover),
-                            borderRadius: BorderRadius.circular(12),
+                            borderRadius: BorderRadius.circular(10),
                             color: Colors.red),
                       ),
-                      Text(popularText[index],style: TextStyle(color: Colors.white,fontFamily: 'gc_m',fontSize: width * 0.045),)
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.copy,
+                                color: buttonColor,
+                                size: width * 0.071,
+                              )),
+                          IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.download,
+                                  color: buttonColor, size: width * 0.071)),
+                          IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.favorite_outline_rounded,
+                                  color: buttonColor, size: width * 0.071)),
+                          IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.share,
+                                  color: buttonColor, size: width * 0.071)),
+                        ],
+                      ),
                     ],
                   ),
-                )),
-              ),
-            ],
+                ),
+                const SizedBox(height: 25),
+                // todo -------------------------> Feelings
+                headingOfHomepage(width, 'Feelings'),
+                SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: List.generate(
+                      feelingsImage.length,
+                          (index) => Column(
+                        children: [
+                          Container(
+                            height: height * 0.13,
+                            width: width * 0.3,
+                            margin: const EdgeInsets.only(
+                                top: 10, right: 12, bottom: 4),
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage(feelingsImage[index]),
+                                    fit: BoxFit.cover),
+                                borderRadius: BorderRadius.circular(12)),
+                          ),
+                          Text(
+                            feelingsText[index],
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'gc_m',
+                                fontSize: width * 0.043),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 25),
+                // todo -------------------------> Family
+                headingOfHomepage(width, 'Family'),
+                SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: List.generate(
+                      familyImage.length,
+                          (index) => Column(
+                        children: [
+                          Container(
+                            height: height * 0.13,
+                            width: width * 0.3,
+                            margin: const EdgeInsets.only(
+                                top: 10, right: 12, bottom: 4),
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage(familyImage[index]),
+                                    fit: BoxFit.cover),
+                                borderRadius: BorderRadius.circular(12)),
+                          ),
+                          Text(
+                            familyText[index],
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'gc_m',
+                                fontSize: width * 0.043),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 25),
+                // todo -------------------------> Moments
+                headingOfHomepage(width, 'Moments'),
+                SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: List.generate(
+                      momentsImage.length,
+                          (index) => Column(
+                        children: [
+                          Container(
+                            height: height * 0.13,
+                            width: width * 0.3,
+                            margin: const EdgeInsets.only(
+                                top: 10, right: 12, bottom: 4),
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage(momentsImage[index]),
+                                    fit: BoxFit.cover),
+                                borderRadius: BorderRadius.circular(12)),
+                          ),
+                          Text(
+                            momentsText[index],
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'gc_m',
+                                fontSize: width * 0.043),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 25),
+                // todo -------------------------> Other
+                headingOfHomepage(width, 'Other'),
+                SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: List.generate(
+                      otherImage.length,
+                          (index) => Column(
+                        children: [
+                          Container(
+                            height: height * 0.13,
+                            width: width * 0.3,
+                            margin: const EdgeInsets.only(
+                                top: 10, right: 12, bottom: 4),
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage(otherImage[index]),
+                                    fit: BoxFit.cover),
+                                borderRadius: BorderRadius.circular(12)),
+                          ),
+                          Text(
+                            otherText[index],
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'gc_m',
+                                fontSize: width * 0.043),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/utils/global.dart
+++ lib/utils/global.dart
@@ -1,21 +1,125 @@
 import 'package:flutter/material.dart';
 
 Color bgColor = const Color(0xff101012);
-
+Color buttonColor = const Color(0xff8A98B7);
+// todo------------------------------> Popular
 List popularImage = [
   'assets/images/HomePage_Images/love.jpg',
-  'assets/images/HomePage_Images/motivation.jpg',
   'assets/images/HomePage_Images/life.jpg',
   'assets/images/HomePage_Images/work.jpg',
   'assets/images/HomePage_Images/mom.jpg',
   'assets/images/HomePage_Images/friends.jpg',
+  'assets/images/HomePage_Images/motivation.jpg',
 ];
 
 List popularText = [
-  'Love',
-  'Motivation',
-  'Life',
-  'Work',
-  'Mom',
-  'Friendship',
+  'Love  ',
+  'Life  ',
+  'Work  ',
+  'Mom  ',
+  'Friendship  ',
+  'Motivation  ',
 ];
+
+// todo------------------------------> Motivation
+List motivationImage = [
+  'assets/images/HomePage_Images/Motivation_Images/inspiration.jpg',
+  'assets/images/HomePage_Images/Motivation_Images/motivation.jpg',
+  'assets/images/HomePage_Images/Motivation_Images/courage.jpg',
+  'assets/images/HomePage_Images/Motivation_Images/freedome.jpg',
+  'assets/images/HomePage_Images/Motivation_Images/intelligence.jpg',
+  'assets/images/HomePage_Images/Motivation_Images/knowledge.jpg',
+];
+
+List motivationText = [
+  'Inspiration  ',
+  'Motivation  ',
+  'Courage  ',
+  'Freedom  ',
+  'Intelligence  ',
+  'Knowledge  ',
+];
+
+// todo------------------------------> Feelings
+List feelingsImage = [
+  'assets/images/HomePage_Images/Feelings_Images/alone.jpg',
+  'assets/images/HomePage_Images/Feelings_Images/sad.jpg',
+  'assets/images/HomePage_Images/Feelings_Images/trust.jpg',
+  'assets/images/HomePage_Images/Feelings_Images/revenge.jpg',
+  'assets/images/HomePage_Images/Feelings_Images/attitude.jpg',
+  'assets/images/HomePage_Images/Feelings_Images/angry.jpg',
+];
+
+List feelingsText = [
+  'Alone  ',
+  'Sad  ',
+  'Trust  ',
+  'Revenge  ',
+  'Attitude  ',
+  'Anger  ',
+];
+
+// todo------------------------------> Family
+List familyImage = [
+  'assets/images/HomePage_Images/Family_Images/family.jpg',
+  'assets/images/HomePage_Images/Family_Images/mom.jpg',
+  'assets/images/HomePage_Images/Family_Images/dad.jpg',
+  'assets/images/HomePage_Images/Family_Images/home.jpg',
+  'assets/images/HomePage_Images/Family_Images/friends.jpg',
+  'assets/images/HomePage_Images/Family_Images/parenting.jpg',
+];
+
+List familyText = [
+  'Family  ',
+  'Mom  ',
+  'Dad  ',
+  'Home  ',
+  'Friends  ',
+  'Parenting  ',
+];
+
+// todo------------------------------> Moments
+List momentsImage = [
+  'assets/images/HomePage_Images/Moments_Images/anniversary.jpg',
+  'assets/images/HomePage_Images/Moments_Images/birthday.jpg',
+  'assets/images/HomePage_Images/Moments_Images/dating.jpg',
+  'assets/images/HomePage_Images/Moments_Images/wedding.jpg',
+  'assets/images/HomePage_Images/Moments_Images/romantic.jpg',
+  'assets/images/HomePage_Images/Moments_Images/death.jpg',
+];
+
+List momentsText = [
+  'Anniversary  ',
+  'Birthday  ',
+  'Romantic  ',
+  'Wedding  ',
+  'Dating  ',
+  'Death  ',
+];
+
+// todo------------------------------> Other
+List otherImage = [
+  'assets/images/HomePage_Images/Other/god.jpg',
+  'assets/images/HomePage_Images/Other/age.jpg',
+  'assets/images/HomePage_Images/Other/change.jpg',
+  'assets/images/HomePage_Images/Other/communication.jpg',
+  'assets/images/HomePage_Images/Other/env.jpg',
+  'assets/images/HomePage_Images/Other/dreams.jpg',
+];
+
+List otherText = [
+  'God  ',
+  'Age  ',
+  'change  ',
+  'communication  ',
+  'environment  ',
+  'dreams  ',
+];
+
+Text headingOfHomepage(double width, String data) {
+  return Text(
+    data,
+    style: TextStyle(
+        color: Colors.white, fontFamily: 'gc_m', fontSize: width * 0.053),
+  );
+}
